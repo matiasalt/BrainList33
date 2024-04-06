efd1935a1d9400128b55f9eb9b4fafc4c7cfc3c9
Jam <mjuanandresm317@gmail.com>
2024-04-06 17:08:34 -0300

    6/4/24

--- app/src/main/java/com/matiasd/brainlist3/Registrar.kt
+++ app/src/main/java/com/matiasd/brainlist3/Registrar.kt
@@ -1,28 +1,20 @@
 package com.matiasd.brainlist3
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class   MainActivity : AppCompatActivity() {
+class Registrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registrar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button=findViewById<Button>(R.id.btnRegister)
-        button.setOnClickListener{
-            val intent=Intent(this,Registrar::class.java)
-            startActivity(intent)
         }
     }
 }
